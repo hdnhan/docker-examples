8497279eea77d2a7cac20f2cd25d0cdf409c266f
Nhan Ho <hdnhan28@gmail.com>
2023-03-19 16:11:50 +0700

    terminate gprc server by SIGINT

--- grpc/example/server/server.cpp
+++ grpc/example/server/server.cpp
@@ -1,10 +1,22 @@
 #include <grpcpp/grpcpp.h>
+#include <signal.h>
 
 #include <iostream>
 #include <memory>
 #include <string>
 
 #include "tunnel.grpc.pb.h"
+
+std::unique_ptr<grpc::Server> server;
+
+// Signal handler function for SIGINT
+void signalHandler(int signal) {
+    std::cout << "Received SIGINT, shutting down server..." << std::endl;
+    // Shut down the gRPC server gracefully
+    server->Shutdown();
+    // Exit the program
+    exit(0);
+}
 
 class BenchServiceImpl final : public tunnel::BenchService::Service {
     grpc::Status StreamData(
@@ -28,8 +40,16 @@ void RunServer() {
     builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
     builder.RegisterService(&service);
 
-    std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+    server = builder.BuildAndStart();
     std::cout << "Server listening on " << server_address << std::endl;
+
+    // Set up the signal handler for SIGINT
+    struct sigaction sigIntHandler;
+    sigIntHandler.sa_handler = signalHandler;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
+    sigaction(SIGINT, &sigIntHandler, NULL);
+
     server->Wait();
 }
 
